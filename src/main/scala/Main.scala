cfe7eedbd13ff2522be23a80c19ffbe223c0e1a8
Kirtirajsinh_Mahida_345 <kirtirajsinh629@gmail.com>
2024-03-05 12:24:31 +0530

    Done

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,11 @@
+import scala.io.Source
+object Main {
+  def main(args: Array[String]): Unit = {
+    val path = "C:/Users/c22755b/IdeaProjects/Employee_Analysis/src/main/scala/Employee.csv"
+    val AllEmployeeDetailsObject = new ParseEmployeeData
+    val AllEmployeeDetails = AllEmployeeDetailsObject.loadDataFromCsv(path)
+    val employeeAnalysis = new PreProcessingAndAnalysingData
+    employeeAnalysis.preProcessingData(AllEmployeeDetails)
+    employeeAnalysis.analysingData(AllEmployeeDetails)
+  }
+}
